f129c4af744bb97531eca5e5f794beebd408012e
kth10421@uos.ac.kr <kth10421@uos.ac.kr>
2021-02-23 21:48:20 +0900

    commit

--- Programmers/3단계_섬_연결하기.py
+++ Programmers/3단계_섬_연결하기.py
@@ -1,28 +1,15 @@
-import collections
 def solution(n, costs):
-    graph = collections.defaultdict(list)
-    for a, b, cost in costs:
-        graph[a].append([b,cost])
-    
-    def to_final(node, visited = [], cost_sum = 0, answer = float("Inf")):
-        print(node, visited, cost_sum, answer)
-        if not graph[node]:
-            if sorted(visited) == sorted(list(range(n))):
-                answer = min(answer, cost_sum)
-                return answer
-            else:
-                return answer
-        else:
-            for next_node, cost in graph[node]:
-                cost_sum += cost
-                answer = to_final(next_node, visited + [node] , cost_sum)
-                print(answer)
-    
-    result = []
-    for i in range(n):
-        result.append(to_final(i))
-    
-    return result
-
-answer = solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]])
-print(answer)
+    answer = 0
+    costs.sort(key = lambda x: x[2])
+    nodes = [costs[0][0]]
+    while len(nodes) != n:
+        for i, cost in enumerate(costs):
+            if (cost[0] in nodes) and (cost[1] in nodes): continue
+            if (cost[0] in nodes) or (cost[1] in nodes):
+                answer += cost[2]
+                nodes.append(cost[0])
+                nodes.append(cost[1])
+                nodes = list(set(nodes))
+                costs.pop(i)
+                break
+    return answer
